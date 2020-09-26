c8c90fa9a6c4a9f284fce7459c898bfe8ee0c0a2
dashkodo <1481570+dashkodo@users.noreply.github.com>
2020-09-26 16:36:08 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+}
+
+a:link, a:visited {
+  font-size: 3em;
+
+  background-color: steelblue;
+  border: solid steelblue;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 10px;
+}
+
+a:hover, a:active {
+	border: solid steelblue;
+  background-color: transparent;
+  color: steelblue;
+}
+
+a.user-name {
+	text-align: center;
+  border: none steelblue;
+  background-color: transparent;
+  color: steelblue;
+  font-size: 3em;
+}
+</style>
+<body>
+<div class="container">
+<a class="user-name">@mcrunok</a>
+<br/>
+<br/>
+<br/>
+  <a class="flex-sm-fill text-sm-center nav-link active" href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQa1y69esqMaZYH8dAvBzxfAQ_RYLafE7K-ROfUAvVFn3_wk0sxOe1zZ2t4iO5I9N6enTxRiE9khcI-/pubhtml?gid=1969368219&single=true">
+  Протокол 11 км</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQa1y69esqMaZYH8dAvBzxfAQ_RYLafE7K-ROfUAvVFn3_wk0sxOe1zZ2t4iO5I9N6enTxRiE9khcI-/pubhtml?gid=0&single=true">Протокол 5 км</a>
+ 
+
+</div>
+</body>
+</html>
